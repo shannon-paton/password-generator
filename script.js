c25d3bd517476c574f478e78f81617cfa919a5b1
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-08-09 19:11:57 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+// generate two random passwords when user clicks the button
+
+// each password should be 15 characters long
+
+
+
+// stretch goals
+
+// ability to set a password length
+// copy-on-click
+// toggle "symbols" and "numbers" on/off
+// dark/light theme switcher
